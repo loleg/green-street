805f5c3d9fdd635456243faf0978410ea63118b4
Oleg Lavrovsky <oleg@utou.ch>
2011-10-11 05:59:34 +0200

    Extended view

--- views/yearly-average/reduce.js
+++ views/yearly-average/reduce.js
@@ -1,3 +1,17 @@
 function(keys, values) {
-  return sum(values) / values.length;
+  var low = 250;
+  var mid = 800;
+  var ll = mm = hh = t = 0;
+  for (var v in values) {
+    t += values[v];
+    if (values[v] < low) {
+      ll++;
+    } else if (values[v] < mid) {
+      mm++;
+    } else {
+      hh++;
+    }
+  }
+  // t == sum(values)
+  return [ll, mm, hh, t/values.length];
 }
